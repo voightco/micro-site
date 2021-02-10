f48d837a39e45d171b4eca035c38e360df37ee21
dandevri <hi@dandevri.es>
2021-02-10 10:53:50 +0100

    Add terser inline js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,23 @@
 const CleanCSS = require("clean-css");
+const { minify } = require("terser");
 
 
 module.exports = function(config) {
   config.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  config.addNunjucksAsyncFilter("jsmin", async function (
+    code,
+    callback
+  ) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error("Terser error: ", err);
+      // Fail gracefully.
+      callback(null, code);
+    }
   });
 }
